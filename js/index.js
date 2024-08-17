f855f67bb5b15b2c0cb3c21b22eb259d8f4f32b2
Juan Lira <dev.juanlira@gmail.com>
2024-08-17 00:36:15 -0300

    update code

--- js/index.js
+++ js/index.js
@@ -29,3 +29,28 @@ document.getElementById('contactForm').addEventListener('submit', function(event
 
     // Aqui você pode adicionar lógica para enviar os dados ao servidor ou outro processamento
 });
+
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + (window.innerHeight * 0.75); // Calcula a posição do topo da janela mais 75% da altura da janela
+    target.forEach(function(element) {
+        if(windowTop > element.offsetTop) {
+            element.classList.add(animationClass);
+        } else {
+            element.classList.remove(animationClass); // Remove a animação se o elemento não estiver mais visível
+        }
+    });
+}
+
+animeScroll()
+
+if(target.length) {
+    window.addEventListener('scroll', function() {
+        animeScroll();
+    })
+   
+}
+
